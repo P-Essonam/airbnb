cda8dd16f467db686b6cf8b7e99bc27c07e043c3
P_essonam <essonammaximin@gmail.com>
2023-11-25 15:56:50 +0100

    added bottom animation data

---
+++ build/js/bottom.js
@@ -0,0 +1,561 @@
+const bottomData= [
+    {
+        1: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        2: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        3: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        4: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        5: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        6: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        7: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        8: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        9: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        10: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        11: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        12: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        13: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        14: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        15: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        16: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        17: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        18: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        
+    },
+
+    {
+        1: {
+           title: 'Aigues',
+           subtitle: 'Locations de vacances',
+        },
+        2: {
+           title: 'Aix',
+           subtitle: 'Locations de vacances',
+        },
+        3: {
+           title: 'Aix-baix',
+           subtitle: 'Locations de chalet',
+        },
+        4: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        5: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances ',
+        },
+        6: {
+           title: 'Avigon du monde',
+           subtitle: 'Locations de vacances',
+        },
+       
+        7: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances ',
+        },
+        8: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        9: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        10: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        11: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances ',
+        },
+        12: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        13: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        14: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        15: {
+           title: 'Aix-les-baix good',
+           subtitle: 'Locations de chalet',
+        },
+        16: {
+           title: 'Amsterdam ',
+           subtitle: 'Locations de vacances',
+        },
+        17: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        18: {
+           title: 'Avigon des maltes',
+           subtitle: 'Locations de vacances wow',
+        },
+       
+        
+    },
+
+    {
+        1: {
+           title: 'Aigues vivantes',
+           subtitle: 'Locations de vacances',
+        },
+        2: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        3: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        4: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances ete',
+        },
+        5: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        6: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        7: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        8: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        9: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet ',
+        },
+        10: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        11: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        12: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        13: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        14: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        15: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        16: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        17: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        18: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances lion',
+        },
+       
+        
+    },
+
+    {
+        1: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        2: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        3: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        4: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        5: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        6: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        7: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        8: {
+           title: 'Aix',
+           subtitle: 'Locations de vacances',
+        },
+        9: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        10: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        11: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        12: {
+           title: 'Avigons de l\'ile',
+           subtitle: 'Locations de vacances',
+        },
+       
+        13: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        14: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        15: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        16: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        17: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        18: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        
+    },
+
+    {
+        1: {
+           title: 'Piscine',
+           subtitle: '',
+        },
+        2: {
+           title: 'Arctique',
+           subtitle: '',
+        },
+        3: {
+           title: 'Camping',
+           subtitle: '',
+        },
+        4: {
+           title: 'Caravanes',
+           subtitle: '',
+        },
+        5: {
+           title: 'Chateaux',
+           subtitle: '',
+        },
+        6: {
+           title: 'Conteneurs maritimes',
+           subtitle: '',
+        },
+       
+        7: {
+           title: 'Campagnes',
+           subtitle: '',
+        },
+        8: {
+           title: 'Design',
+           subtitle: '',
+        },
+        9: {
+           title: 'Maison oraganiques',
+           subtitle: '',
+        },
+        10: {
+           title: 'Fermes',
+           subtitle: '',
+        },
+        11: {
+           title: 'Parcs nationaux',
+           subtitle: '',
+        },
+        12: {
+           title: 'Vignobles',
+           subtitle: '',
+        },
+       
+        13: {
+           title: 'wow !',
+           subtitle: '',
+        },
+        14: {
+           title: 'Tinny houses',
+           subtitle: '',
+        },
+        15: {
+           title: 'Tours',
+           subtitle: '',
+        },
+        16: {
+           title: 'Moulins a vent',
+           subtitle: '',
+        },
+        17: {
+           title: '',
+           subtitle: '',
+        },
+        18: {
+           title: '',
+           subtitle: '',
+        },
+       
+        
+    },
+
+    {
+        1: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        2: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances avec piscine',
+        },
+        3: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        4: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        5: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        6: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        7: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        8: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        9: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        10: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        11: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        12: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        13: {
+           title: 'Aigues morte',
+           subtitle: 'Locations de vacances',
+        },
+        14: {
+           title: 'Aix-en-provences',
+           subtitle: 'Locations de vacances',
+        },
+        15: {
+           title: 'Aix-les-baix',
+           subtitle: 'Locations de chalet',
+        },
+        16: {
+           title: 'Amsterdam',
+           subtitle: 'Locations de vacances',
+        },
+        17: {
+           title: 'Arzon',
+           subtitle: 'Locations de vacances',
+        },
+        18: {
+           title: 'Avigon',
+           subtitle: 'Locations de vacances',
+        },
+       
+        
+    },
+
+];
+
+export function F_bottomData(data,jsUl,titles) {
+
+   let contentBottomHTML = '';
+
+   jsUl.forEach((element,index) => {
+       element.addEventListener('click', function () {
+           contentBottomHTML =  `
+           <div class="font-bold text-base">${bottomData[index][1].title} <span class="font-normal block">${bottomData[index][1].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][2].title}  <span class="font-normal block">${bottomData[index][2].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][3].title}  <span class="font-normal block">${bottomData[index][3].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][4].title} <span class="font-normal block">${bottomData[index][4].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][5].title}  <span class="font-normal block">${bottomData[index][5].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][6].title}  <span class="font-normal block">${bottomData[index][6].subtitle}</span></div>
+       
+       
+           <div class="font-bold text-base">${bottomData[index][7].title} <span class="font-normal block">${bottomData[index][7].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][8].title}  <span class="font-normal block">${bottomData[index][8].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][9].title}  <span class="font-normal block">${bottomData[index][9].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][10].title} <span class="font-normal block">${bottomData[index][10].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][11].title}  <span class="font-normal block">${bottomData[index][11].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][12].title}  <span class="font-normal block">${bottomData[index][12].subtitle}</span></div>
+       
+       
+           <div class="font-bold text-base">${bottomData[index][13].title} <span class="font-normal block">${bottomData[index][13].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][14].title}  <span class="font-normal block">${bottomData[index][14].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][15].title}  <span class="font-normal block">${bottomData[index][15].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][16].title} <span class="font-normal block">${bottomData[index][16].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][17].title}  <span class="font-normal block">${bottomData[index][17].subtitle}</span></div>
+           <div class="font-bold text-base">${bottomData[index][18].title}  <span class="font-normal block">${bottomData[index][18].subtitle}</span></div>
+       
+
+           `;
+           data.innerHTML = contentBottomHTML;
+           
+           titles.forEach((element,ind) => {
+               if (ind !== index) {
+                   titles[ind].classList.add('hidden');
+                   jsUl[ind].classList.add('removePointer');
+                   if (titles[index].classList.contains('hidden')) {
+                       titles[index].classList.toggle('hidden');
+                   }else{
+                     jsUl[index].classList.remove('removePointer');
+                   }
+                   
+               }
+               
+           });
+
+           console.log(jsUl[index].classList)
+           
+       })
+   }); 
+
+   if (!contentBottomHTML) {
+       contentBottomHTML =  `
+           
+               <div class="font-bold text-base">${bottomData[0][1].title} <span class="font-normal block">${bottomData[0][1].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][2].title}  <span class="font-normal block">${bottomData[0][2].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][3].title}  <span class="font-normal block">${bottomData[0][3].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][4].title} <span class="font-normal block">${bottomData[0][4].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][5].title}  <span class="font-normal block">${bottomData[0][5].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][6].title}  <span class="font-normal block">${bottomData[0][6].subtitle}</span></div>
+           
+           
+               <div class="font-bold text-base">${bottomData[0][7].title} <span class="font-normal block">${bottomData[0][7].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][8].title}  <span class="font-normal block">${bottomData[0][8].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][9].title}  <span class="font-normal block">${bottomData[0][9].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][10].title} <span class="font-normal block">${bottomData[0][10].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][11].title}  <span class="font-normal block">${bottomData[0][11].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][12].title}  <span class="font-normal block">${bottomData[0][12].subtitle}</span></div>
+           
+           
+               <div class="font-bold text-base">${bottomData[0][13].title} <span class="font-normal block">${bottomData[0][13].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][14].title}  <span class="font-normal block">${bottomData[0][14].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][15].title}  <span class="font-normal block">${bottomData[0][15].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][16].title} <span class="font-normal block">${bottomData[0][16].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][17].title}  <span class="font-normal block">${bottomData[0][17].subtitle}</span></div>
+               <div class="font-bold text-base">${bottomData[0][18].title}  <span class="font-normal block">${bottomData[0][18].subtitle}</span></div>
+           
+       `;
+   data.innerHTML = contentBottomHTML;
+   titles[0].classList.toggle('hidden');
+   }
+}
--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,14 @@
+import { F_bottomData } from "./bottom.js";
+
 const app = () => {
-
     
-
+    const data = document.querySelector('.js-bottom-data');
+    const jsUl = document.querySelectorAll('.js-ul li');
+    const titles = document.querySelectorAll('.title li div');
+ 
+    F_bottomData(data,jsUl,titles);
 
 };
 
-document.addEventListener('DOMContentLoaded', app)
+document.addEventListener('DOMContentLoaded', app);
+
