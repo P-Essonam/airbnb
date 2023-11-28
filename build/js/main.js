fe2890afaae2eba45ea8f2a013bf516d159d4537
P_essonam <essonammaximin@gmail.com>
2023-11-28 13:38:36 +0100

    all to all

--- build/js/main.js
+++ build/js/main.js
@@ -11,6 +11,23 @@ const app = () => {
 
     sp();
 
+    function cacherElement() {
+        let seuil = document.getElementById('maDiv').getBoundingClientRect().top;
+        let monElement = document.getElementById('monElement');
+  
+        // Vérifier la position actuelle de défilement par rapport à la position de la div
+        if (window.scrollY >= seuil) {
+          monElement.style.display = 'none'; // Cacher l'élément
+        } else {
+          monElement.style.display = 'flex'; // Afficher l'élément
+        }
+
+        console.log(seuil);
+    }
+  
+      // Écouter l'événement de défilement
+    window.addEventListener('scroll', cacherElement);
+
 };
 
 document.addEventListener('DOMContentLoaded', app);
